0e1d58e2b1015c45a479e190e4f36bb9d0360373
rrufai <rrufai@gmail.com>
2019-10-30 07:46:11 -0400

    cleanup

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,7 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -41,4 +42,4 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    unittest.main()
